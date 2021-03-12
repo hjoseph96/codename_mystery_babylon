71d01213f73682d7c63080d0acc4b547080b60a9
Herbert Joseph <techniquea2z@gmail.com>
2021-03-12 16:35:33 -0500

    Setup MAgician Battler --- add multi-attacks for mages

--- Assets/Scripts/Core/Units/Battlers/Magic Users/MagicEffect.cs
+++ Assets/Scripts/Core/Units/Battlers/Magic Users/MagicEffect.cs
@@ -14,17 +14,19 @@ public class MagicEffect : MonoBehaviour
     public MagicEffectType EffectType;
     public float moveSpeed = 5f;
     public GameObject hitEffect;
+    [SoundGroupAttribute] public string movingSound;
     [SoundGroupAttribute] public string hitSound;
     [HideInInspector] public Action OnHitTarget;
 
-    private Transform _target;
+    private Vector3 _target;
     private bool _startedMoving = false;
 
     
-    public void StartMoving(Transform target)
+    public void StartMoving(Vector3 target)
     {
         _target = target;
         _startedMoving = true;
+        MasterAudio.PlaySound3DFollowTransform(movingSound, CampaignManager.AudioListenerTransform);
     }
 
     // Update is called once per frame
@@ -39,8 +41,12 @@ public class MagicEffect : MonoBehaviour
         if (EffectType == MagicEffectType.Projectile)
         {
             Destroy(this.gameObject);
-            var shownHitEffect = Instantiate(hitEffect, this.transform.position, hitEffect.transform.rotation).GetComponent<ParticleSystem>();
-            Destroy(shownHitEffect, shownHitEffect.main.startLifetime.constant + shownHitEffect.main.duration);
+            var shownHitEffect = Instantiate(hitEffect, this.transform.position, hitEffect.transform.rotation).GetComponent<ParticleLifetimeEvents>();
+            
+            shownHitEffect.ParticleDied += delegate() {
+                Destroy(shownHitEffect);
+            };
+            MasterAudio.PlaySound3DFollowTransform(hitSound, CampaignManager.AudioListenerTransform);
 
             OnHitTarget.Invoke();
         }       
@@ -59,7 +65,7 @@ public class MagicEffect : MonoBehaviour
                 this.transform.LookAt(_target, Vector3.up);
                 this.transform.position = Vector3.Slerp(
                     this.transform.position,
-                    _target.position,
+                    _target,
                     moveSpeed * Time.deltaTime
                 );
 
